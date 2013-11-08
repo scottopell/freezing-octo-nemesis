421c13c353c75d568e7f62c861827df1f3eb2048
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2013-11-08 12:03:08 -0500

    Scott sucks so much

--- SendMessage.java
+++ SendMessage.java
@@ -1,6 +1,6 @@
 public class SendMessage{
   public static void main(String args[]){
-    System.out.println("Kirby Sucks");
-    bug
+    System.out.println("Scott Sucks a lot");
+    System.out.println("Kirby doesn't suck!");
   }
 }
